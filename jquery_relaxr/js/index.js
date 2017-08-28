18769c2723886554a2bce318f775bfa11a724a49
Yogesh Phadke <YLPhadke@gmail.com>
2017-08-28 13:02:16 -0400

    relaxr_jquery

---
+++ jquery_relaxr/js/index.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function() {
+  $('.readmore').click(highlightRed);
+  $('.readmore.a').click(highlightRed);
+  $(".readless").click(function(){
+    $("p").slideUp();
+  $(".readmore").click(function(){
+    $("p").slideDown();
+  $(".readmore").click(function(){
+    $("p").hide();
+  $(".readmore.a").click(function(){
+    $("p").show();
+  $("posts").click(function(){
+    $("p").hide();
+  $("#post").click(function(){
+    $("p").show();
+
+});
+
+
+
+
+
+
