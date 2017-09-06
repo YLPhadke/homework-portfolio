645f0a9361f6759cf6b3323f541841f902b7e755
Yogesh Phadke <YLPhadke@gmail.com>
2017-09-06 13:53:37 -0400

    CityPix assignment

---
+++ Sketch/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('#blue-link').click(function (e) {
+    e.preventDefault();
+    $('body').css('background', 'blue');
+  });
+});
---
+++ homework-city/starter_code/js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("a").click(function(event){
+        event.preventDefault();
+
+$(document).ready(function() {
+  $('#enterCity').submit(enter);
+
+function enterCity() {
+  var enterCity = $('#changeBackground').val();
+
+function changeBackground() {
+  if(input == 'New York' || "NYC") { // When a user enters "New York" or "NYC"
+    imageSrc = 'images/nyc.jpg'; // Make background nyc.jpg
+  if(input == 'Bay Area' || "SF" || "San Fransisco") // When a user enters "San Fransisco" or "SF" or "Bay Area"
+    imageSrc = 'images/sf.jpg'; // Make the background sf.jpg
+  if(input == 'Los Angeles' || "LA" || "LAX") // When a user enters "Log Angeles" or "LA" or "LAX"
+    imageSrc = 'images/la.jpg'; // Make the background la.jpg
+  if(input == 'Austin' || "ATX" ) // When a user enters "Austin" or "ATX"
+    imageSrc = 'images/atx.jpg'; // Make the background austin.jpg
+  if(input == 'Sydney' || "SYD") // When a user enters "Sydney" or "SYD"
+    imageSrc = 'images/syd.jpg'; // Make the backround of the page sydney.jpg
+
+
+
+
+}
+
+
+});
+
+
